d186d2cf3158b5e71d64134ff9650a34018e0aea
Steica <steica@live.com>
2023-07-30 12:22:14 +0300

    PBInfo #890 NrVocale

--- main.cpp
+++ main.cpp
@@ -3,24 +3,42 @@
 
 using namespace std;
 
-bool isVowel (char str) {
-    return (str == 'a' || str == 'e' || str == 'i' || str == 'o' || str == 'u');
+bool isVowel(char chr) {
+    return (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u' ||
+            chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U');
 }
 
 int main() {
-    char str[101], *p;
-    cin.getline(str, 101);
-    p = strtok(str, " ,");
-    while (p != NULL) {
-        int cnt_voWel = 0;
-        cout << p << " " << strlen(p) << "\n";
-        for (int i = 0; i < strlen(p); i++) {
-            if (isVowel(p[i]))
-                cnt_voWel++;
-        }
-        cout << " " << cnt_voWel << endl;
-        p = strtok(NULL, " ,");
+    char str[256];
+    int frecv[5]{}, maxim = 0, litera;
+    cin.getline (str, 256);
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == 'a' || str[i] == 'A')
+            frecv[0]++;
+        if (str[i] == 'e' || str[i] == 'E')
+            frecv[1]++;
+        if (str[i] == 'i' || str[i] == 'I')
+            frecv[2]++;
+        if (str[i] == 'o' || str[i] == 'O')
+            frecv[3]++;
+        if (str[i] == 'u' || str[i] == 'U')
+            frecv[4]++;
     }
+    for (int i = 0; i <= 4; i++)
+        if (frecv[i] > maxim) {
+            maxim = frecv[i];
+            litera = i;
+        }
+    if (litera == 0)
+        cout << "A";
+    else if (litera == 1)
+        cout << "E";
+    else if (litera == 2)
+        cout << "I";
+    else if (litera == 3)
+        cout << "O";
+    else
+        cout << "U";
 }
 
 
