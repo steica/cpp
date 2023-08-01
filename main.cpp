70604cec35fb09c976d44a8f8eccd1a548ccbf66
Steica <steica@live.com>
2023-08-01 17:51:15 +0300

    PBInfo #2635 CapsLock

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
 
 using namespace std;
 
-bool isVowel(char chr) {
-    return (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u');
-}
-
+ifstream fin("capslock.in");
+ofstream fout("capslock.out");
 
 int main() {
-    char str[101];
-    cin.getline(str, 101);
-    bool exista_vocale = false;
-    for (int i = 0; str[i]; i++) {
-        if (isVowel(str[i]))
-            exista_vocale = true;
-    }
-    if (exista_vocale) {
-        for (int i = 0; str[i]; i++) {
-            cout << str[i];
-            if (isVowel(str[i]))
-                cout << "*";
+    char str[256];
+    fin.getline(str, 256);
+    bool CAPS = false;
+    for (int i = 0; str[i]; i++)
+        if (str[i] == '#')
+            CAPS = !CAPS;
+        else if (str[i] == ' ')
+            fout << str[i];
+        else {
+            if (CAPS)
+                str[i] -= 32;
+            fout << str[i];
         }
-    }
-    else
-        cout << "FARA VOCALE";
 }
+
