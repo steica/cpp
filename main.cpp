9b96d67fe10353728e894dbc7b53c3389d3af4b3
Steica <steica@live.com>
2023-08-01 12:47:44 +0300

    PBinfo #85 InserareAsterisc

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@
 using namespace std;
 
 bool isVowel(char chr) {
-    return (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u' ||
-            chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U');
+    return (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u');
 }
 
 
 int main() {
-    char str[11];
-    cin >> str;
-    int ind_vocala = -1, ind_consoana = -1;
+    char str[101];
+    cin.getline(str, 101);
+    bool exista_vocale = false;
     for (int i = 0; str[i]; i++) {
-        if (isVowel(str[i]) && ind_vocala == -1)
-            ind_vocala = i;
-        if (!isVowel(str[i]))
-            ind_consoana = i;
+        if (isVowel(str[i]))
+            exista_vocale = true;
     }
-    swap(str[ind_vocala], str[ind_consoana]);
-    if (ind_vocala != -1 && ind_consoana != -1)
-        cout << str;
+    if (exista_vocale) {
+        for (int i = 0; str[i]; i++) {
+            cout << str[i];
+            if (isVowel(str[i]))
+                cout << "*";
+        }
+    }
     else
-        cout << "IMPOSIBIL";
+        cout << "FARA VOCALE";
 }
