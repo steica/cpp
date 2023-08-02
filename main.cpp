e52d7ee835ebda432dc7e42cddfbfd9e9929b58d
Steica <steica@live.com>
2023-08-02 11:42:41 +0300

    PBinfo #90 NumarMaximVocale

--- main.cpp
+++ main.cpp
@@ -4,22 +4,30 @@
 
 using namespace std;
 
-ifstream fin("capslock.in");
-ofstream fout("capslock.out");
+bool isVowel (char chr) {
+    return chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u';
+}
+
+ifstream fin("vocmax.in");
+ofstream fout("vocmax.out");
 
 int main() {
-    char str[256];
-    fin.getline(str, 256);
-    bool CAPS = false;
-    for (int i = 0; str[i]; i++)
-        if (str[i] == '#')
-            CAPS = !CAPS;
-        else if (str[i] == ' ')
-            fout << str[i];
-        else {
-            if (CAPS)
-                str[i] -= 32;
-            fout << str[i];
+    int n, indice;
+    char str[100][251];
+    fin >> n;
+    int maxim = 0;
+    for (int i = 0; i <= n; i++) {
+        fin.getline(str[i],251);
+        int cnt_vowel = 0;
+        for (int j = 0; str[i][j]; j++) {
+            if (isVowel(str[i][j]))
+                cnt_vowel++;
         }
+        if (cnt_vowel > maxim) {
+            maxim = cnt_vowel;
+            indice = i;
+        }
+    }
+    fout << str[indice];
 }
 
