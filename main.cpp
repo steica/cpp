b4e6dd1309afed372e871319723258e3e4b30044
Steica <steica@live.com>
2023-09-03 14:02:11 +0300

    PBInfo #429 CifBin

--- main.cpp
+++ main.cpp
@@ -3,28 +3,15 @@
 using namespace std;
 
 int main() {
-    int x, y;
-    cin >> x >> y;
-    int cop_x = x, cop_y = y;
-    int cif_1_x = 0, cif_1_y = 0;
-    while (x) {
-        if (x % 2 == 1)
-            cif_1_x++;
-        x /= 2;
-    }
-    while (y) {
-        if (y % 2 == 1)
-            cif_1_y++;
-        y /= 2;
-    }
-   if (cif_1_x == cif_1_y) {
-        if (x < y)
-            cout << cop_x;
+    int n;
+    cin >> n;
+    int Z = 0, U = 0;
+    while (n) {
+        if (n % 2 == 0)
+            Z++;
         else
-            cout << cop_y;
+            U++;
+        n /= 2;
     }
-    else if (cif_1_y > cif_1_x)
-        cout << cop_y;
-    else
-        cout << cop_x;
+    cout << Z << " " << U;
 }
