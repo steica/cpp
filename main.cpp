ca48ac711e2e64f8c1b4c64a50553a9fb3c648bf
Steica <steica@live.com>
2023-09-06 14:42:21 +0300

    PBInfo #946 douabaze

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
+char c[101];
+int n, i, a[101], x;
 
 int main() {
-    long long b, n, p;
-    cin >> b >> n >> p;
-    long long result = 1;
-    while (n) {
-        if (n % 2 == 1) {
-            result = (b * result) % (long long)pow(10, p);
-        }
-        b = (b * b) % (long long) pow(10, p);
-        n /= 2;
+
+    cin >> c;
+    n = 0;
+    while (c[n] >= '0' && c[n] <= '9') {
+        a[n] = c[n] - '0';
+        n++;
     }
-    cout << result;
+    n--;
+    if ((n + 1) % 2 == 1) {
+        cout << 1;
+        for (i = 1; i < n; i = i + 2) {
+            x = a[i] * 2 + a[i + 1];
+            cout << x;
+        };
+    } else
+        for (i = 0; i < n; i = i + 2)
+            cout << a[i] * 2 + a[i + 1];
+
+    return 0;
 }
-
-
-
-
 
