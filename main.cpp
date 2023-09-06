1ef7c26b90d62f18215ec494d30c3b1168fdeca6
Steica <steica@live.com>
2023-09-06 22:04:01 +0300

    PBInfo #2580 setbit0

--- main.cpp
+++ main.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 int main() {
-    int T, b;
-    uint64_t n;
-    cin >> T;
-    for (int i = 0; i < T; i++) {
-        cin >> n >> b;
-        cout << ((n >> b) & 1);
-    }
+    int64_t n;
+    int b;
+    cin >> n >> b;
+    n = n & ~ (1 << b);
+    cout << n;
 }
