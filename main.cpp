52efbef3d20a069df6324f01d62da4530752b253
Steica <steica@live.com>
2023-09-08 12:31:39 +0300

    PBInfo #4382 Inlocuire5

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-bool estePrim(int n) {
+/*bool estePrim(int n) {
     if (n < 2)
         return false;
     for (int d = 2; d * d <= n; d++) {
@@ -11,6 +11,7 @@ bool estePrim(int n) {
     }
     return true;
 }
+ */
 
 int main() {
     int n, a[1000];
@@ -19,11 +20,18 @@ int main() {
         cin >> a[i];
     }
     for (int i = n - 1; i >= 0; i--) {
-        if (estePrim(a[i])) {
+        bool este_prim = true;
+        if (a[i] < 2)
+            este_prim = false;
+        for (int d = 2; d * d <= a[i]; d++) {
+            if (a[i] % d == 0)
+                este_prim = false;
+        }
+        if (este_prim) {
             a[i] = 0;
-            break;
         }
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
+    }
 }
