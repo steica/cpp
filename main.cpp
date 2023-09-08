a30000cd4be7d2725bc67a31ee22aede4e1fcb5f
Steica <steica@live.com>
2023-09-08 12:17:21 +0300

    #4383 Inlocuire6

--- main.cpp
+++ main.cpp
@@ -2,17 +2,28 @@
 
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    int cnt_par = 0, cnt_impar = 0;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        if (x % 2 == 1)
-            cnt_impar++;
-        else
-            cnt_par++;
+bool estePrim(int n) {
+    if (n < 2)
+        return false;
+    for (int d = 2; d * d <= n; d++) {
+        if (n % d == 0)
+            return false;
     }
-    cout << abs(cnt_par - cnt_impar);
+    return true;
+}
+
+int main() {
+    int n, a[1000];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        if (estePrim(a[i])) {
+            a[i] = 0;
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
 }
