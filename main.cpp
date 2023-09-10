a201b012af223af0f2f0ea1c19f961ee05f0be31
Steica <steica@live.com>
2023-09-10 16:33:33 +0300

    PBInfo #922 Puncte

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-struct Fractie {
-    int numarator, numitor;
-} F1, F2;
-
-int cmmmdc (int x, int y) {
-    int rest;
-    while (y) {
-        rest = x % y;
-        x = y;
-        y = rest;
-    }
-    return x;
-}
+struct Coordonate {
+    double abcisa, ordonata;
+} axa;
 
 int main() {
-    cin >> F1.numarator >> F1.numitor >> F2.numarator >> F2.numitor;
-    if (F1.numarator / F1.numitor > F2.numarator / F2.numitor)
-        cout << F1.numarator / cmmmdc(F1.numarator, F1.numitor) << " " << F1.numitor / cmmmdc(F1.numarator, F1.numitor);
-    else
-        cout << F2.numarator / cmmmdc(F2.numarator, F2.numitor) << " " << F2.numitor / cmmmdc(F2.numarator, F2.numitor);
+    int n;
+    cin >> n;
+    double distanta, distanta_max = 0;
+    int numar_puncte = 0;
+    double a[100]{};
+    int ind = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> axa.abcisa >> axa.ordonata;
+        distanta = sqrt (pow(axa.abcisa, 2) + pow(axa.ordonata, 2));
+        a[ind++] = distanta;
+        if (distanta_max < distanta)
+            distanta_max = distanta;
+    }
+    for (int i = 0; i < ind; i++) {
+        if (a[i] == distanta_max)
+            numar_puncte++;
+    }
+    cout << fixed << setprecision(3) << distanta_max << " " << numar_puncte;
+
 }
