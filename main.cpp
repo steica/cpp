61c0d803e5aca6d2543c966bda4eef5d935c0ad7
Steica <steica@live.com>
2023-09-14 13:28:22 +0300

    PNInfo #3481 sort_div

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
-/*#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <cstring>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 ifstream fin("sort_div.in");
 ofstream fout("sort_div.out");
 
-struct numar{
+struct numar {
     int nr_div, cif_control, prima_cifra, valoare;
 } a[10001];
 
-int NumarDivizori (int x) {
+int NumarDivizori(int x) {
     int cnt_div = 0;
     for (int d = 1; d <= sqrt(x); d++) {
         if (x % d == 0) {
@@ -22,29 +23,10 @@ int NumarDivizori (int x) {
                 cnt_div++;
         }
     }
+    return cnt_div;
 }
 
-int CifraControl (int x) {
-    int cifra_control = 0;
-    while (x) {
-        cifra_control += x % 10;
-        x /= 10;
-    }
-}
-
-
-int main() {
-    int n;
-    fin >> n;
-
-}
-
-*/
-#include <iostream>
-
-using namespace std;
-
-int cifraControl (int x) {
+int cifraControl(int x) {
     if (x < 10)
         return x;
     int sumaCifrelor = 0;
@@ -55,8 +37,41 @@ int cifraControl (int x) {
     return cifraControl(sumaCifrelor);
 }
 
+int primaCifra(int x) {
+    while (x > 9)
+        x /= 10;
+    return x;
+}
+
+void citire(numar &n) {
+    fin >> n.nr_div >> n.cif_control >> n.prima_cifra >> n.valoare;
+}
+
+int comparare (numar a, numar b) {
+    if(a.nr_div < b.nr_div)
+        return 1;
+    else if (a.nr_div == b.nr_div && a.cif_control < b.cif_control)
+        return 1;
+    else if (a.nr_div == b.nr_div && a.cif_control == b.cif_control && a.prima_cifra < b.prima_cifra)
+        return 1;
+    else if (a.nr_div == b.nr_div && a.cif_control == b.cif_control && a.prima_cifra == b.prima_cifra && a.valoare < b.valoare)
+        return 1;
+    else
+        return 0;
+}
+
 int main() {
     int n;
-    cin >> n;
-    cout << cifraControl(n);
+    fin >> n;
+    for (int i = 1; i <= n; i++) {
+        fin >> a[i].valoare;
+        a[i].nr_div = NumarDivizori(a[i].valoare);
+        a[i].cif_control = cifraControl(a[i].valoare);
+        a[i].prima_cifra = primaCifra(a[i].valoare);
+    }
+    sort (a + 1, a + n + 1, comparare);
+    for (int i = 1; i <= n; i++)
+        fout << a[i].valoare << " ";
 }
+
+
