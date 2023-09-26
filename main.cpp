414d640d4bf70d12a992aae20037e49d21a89a33
Steica <steica@live.com>
2023-09-26 17:23:08 +0300

    PBInfo #975 Cuvinte2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,3 @@
-/*#include <iostream>
-#include <cstring>
-
-using namespace std;
-
-int main() {
-    char str[256];
-    cin.getline(str, 255);
-    char sep[ ] = " ";
-    char voc[ ] = "aeiou";
-
-    char *pointer = strtok(str, sep);
-    while (pointer) {
-        bool OK = true;
-        for (int i = 0; pointer[i]; i++)
-            if (!strchr(voc, pointer[i]))
-                OK = false;
-            if (OK)
-                cout << pointer << "\n";
-            pointer = strtok(NULL, sep);
-    }
-}*/
-
 #include <iostream>
 #include <cstring>
 
@@ -28,19 +5,40 @@ using namespace std;
 
 int main() {
     char str[256];
-    cin.getline(str, 255);
-    char separator[] = ", .;";
-    char *pointer;
-    char vocale[] = "aeiou";
-    pointer = strtok(str, separator);
+    cin.getline(str, 256);
+    char sep[] = " ,.";
+    char matrice[255][11];
+    int ind = 0;
+    char *pointer = strtok(str, sep);
+
     while (pointer) {
-        bool ok = true;
-        for (int i = 0; pointer[i]; i++) {
-            if (!strchr(vocale, pointer[i]))
-                ok = false;
-        }
-        if (ok)
-            cout << pointer << "\n";
-        pointer = strtok(NULL, separator);
+            strcpy(matrice[++ind], pointer);
+            pointer = strtok(NULL, sep);
     }
+
+    for (int i = 1; i < ind; ++i) {
+        for (int j = i + 1; j <= ind; ++j) {
+            if (strcmp(matrice[i], matrice[j]) > 0) {
+                char aux[11];
+                strcpy(aux, matrice[i]);
+                strcpy(matrice[i], matrice[j]);
+                strcpy(matrice[j], aux);
+            }
+        }
+    }
+    bool gasit = false;
+    for (int i = 1; i <= ind && !gasit; ++i) {
+        bool palindrom = true;
+        int stg = 0, dr = strlen(matrice[i]) - 1;
+        for ( ; stg <= dr; ++stg, --dr) {
+            if (matrice[i][stg] != matrice[i][dr])
+                palindrom = false;
+        }
+        if (palindrom) {
+            gasit = true;
+            cout << matrice[i];
+        }
+    }
+    if(!gasit)
+        cout << "IMPOSIBIL";
 }
