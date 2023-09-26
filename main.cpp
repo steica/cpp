291c41f55b40aaa0425fac4d30cd20bccb75a134
Steica <steica@live.com>
2023-09-26 10:40:31 +0300

    PBInfo #973 Cuvinte1

--- main.cpp
+++ main.cpp
@@ -1,25 +1,46 @@
+/*#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char str[256];
+    cin.getline(str, 255);
+    char sep[ ] = " ";
+    char voc[ ] = "aeiou";
+
+    char *pointer = strtok(str, sep);
+    while (pointer) {
+        bool OK = true;
+        for (int i = 0; pointer[i]; i++)
+            if (!strchr(voc, pointer[i]))
+                OK = false;
+            if (OK)
+                cout << pointer << "\n";
+            pointer = strtok(NULL, sep);
+    }
+}*/
+
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
 int main() {
-   char a[101][101];
-   int n;
-   cin >> n;
-   for (int i = 0; i < n; i++)
-       cin >> a[i];
-   for (int i = 0; i < n - 1; i++) {
-       for (int j = i + 1; j < n; j++) {
-           if (strcmp(a[i], a[j]) > 0) {
-               char dest[101];
-               strcpy(dest, a[i]);
-               strcpy(a[i], a[j]);
-               strcpy(a[j], dest);
-           }
-       }
-   }
-   for (int i = 0; i < n; i++)
-       cout << a[i] << endl;
+    char str[256];
+    cin.getline(str, 255);
+    char separator[] = ", .;";
+    char *pointer;
+    char vocale[] = "aeiou";
+    pointer = strtok(str, separator);
+    while (pointer) {
+        bool ok = true;
+        for (int i = 0; pointer[i]; i++) {
+            if (!strchr(vocale, pointer[i]))
+                ok = false;
+        }
+        if (ok)
+            cout << pointer << "\n";
+        pointer = strtok(NULL, separator);
+    }
 }
-
