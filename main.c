5e1bc95c2fd5f78556ff46a31a7e09dbf043822f
Steica <steica@live.com>
2023-09-27 13:14:39 +0300

    PBInfo #3112 Bac_Cuvinte

--- main.c
+++ main.c
@@ -2,14 +2,27 @@
 #include <string.h>
 
 int main(void) {
-    char a[251], b[251];
-    scanf("%s\n%s", a, b);
-    for (int i = 0; a[i]; i++) {
-        for (int j = 0; b[j]; j++) {
-            if (a[i] == b[j]) {
-                printf("%c", a[i]);
-                break;
+    char str[102];
+    fgets(str, sizeof(str), stdin);
+    int n;
+    scanf("%d", &n);
+    int ind = 0;
+    int gasit = 0, i = 0;
+    for (i = 0; str[i]; i++) {
+        if (str[i] == ' ' || str[i] == '\n') {
+            if (i - ind == n) {
+                gasit = 1;
+                for (int j = ind; j < i; j++)
+                    printf("%c", str[j]);
+                printf("\n");
             }
+            ind = i + 1;
         }
     }
+    if (!gasit)
+        printf("nu exista");
 }
+
+
+//str[i] = '\0';
+//printf("%s", str + ind)
