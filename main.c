6e7d96ee8bfdf8798a0edec727476b8e4769df20
Steica <steica@live.com>
2023-10-01 11:06:33 +0300

    PBInfo #875 Stiva - cu functii

--- main.c
+++ main.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
+int stiva[1000] = {};
+int height_stiva = 0;
+
+int isEmpty() {
+    return height_stiva == 0;
+}
+
+void pop() {
+    if (!isEmpty())
+        height_stiva--;
+}
+
+void push(int x) {
+    stiva[height_stiva++] = x;
+}
+
+int top() {
+    if (!isEmpty())
+        return stiva[height_stiva - 1];
+}
+
 int main(void) {
     int n;
     scanf("%d", &n);
     int elem;
-    int stiva[1000] = {};
     char str[5];
-    int height_stiva = 0;
     for (int i = 0; i < n; i++) {
         scanf("%s", str);
-        if (strcmp(str, "top") == 0) {
-            if (height_stiva > 0)
-                printf("%d\n", stiva[height_stiva - 1]);
-        } else if (strcmp(str, "pop") == 0) {
-            if (height_stiva > 0)
-                height_stiva--;
-        } else {
+        if (strcmp(str, "push") == 0) {
             scanf("%d", &elem);
-            stiva[height_stiva++] = elem;
-        }
+            push(elem);
+        } else if (strcmp(str, "pop") == 0) {
+            pop();
+        } else
+            printf("%d\n", top());
     }
+    return 0;
 }
