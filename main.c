dc776c0edc92cd0dd51636e533596dbd19ed203a
Steica <steica@live.com>
2023-10-04 13:10:43 +0300

    PBInfo #3795 Paranteze4

--- main.c
+++ main.c
@@ -1,23 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-    int n, latura[1001];
-    scanf("%d", &n);
-    int stiva[1000];
-    int height_stiva = 0;
-    for (int i = 1; i <= n; i++){
-        scanf("%d", &latura[i]);
-        while (height_stiva > 0 && latura[i] > latura[stiva[height_stiva - 1]])
-            height_stiva--;
-        stiva[height_stiva++] = i;
-    }
-    printf("%d\n", height_stiva);
-    for (int i = 0; i < height_stiva; i++)
-        printf("%d ", stiva[i]);
+
+int stack[100], dim = 0;
+
+void push(int x) {
+    stack[dim++] = x;
 }
 
-/*int isEmpty() {
+void pop() {
+    dim--;
+    printf("%d ", stack[dim]);
+}
+
+int main() {
+    char str[256];
+    scanf("%s", str);
+    int ctr = 0;
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '(') {
+            ctr++;
+            push(ctr);
+        } else {
+            pop(dim);
+        }
+    }
+}
+
+
+/*#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char str[255];
+    char stiva[255];
+    int rezultate[255], ind = 0;
+    scanf("%s", str);
+    int height_stiva = 0;
+    for (int j = 1; str[j]; j++) {
+        if (height_stiva == 0)
+            stiva[height_stiva++] = str[j];
+        else {
+            if (stiva[height_stiva - 1] == '(' && str[j] == ')') {
+                height_stiva--;
+                rezultate[ind++] =;
+            } else {
+                stiva[height_stiva++] = str[j];
+            }
+        }
+    }
+    for (int i = 0; i < ind; i++)
+        printf("%d ", rezultate[i]);
+}*/
+
+
+
+/*
+int isEmpty() {
     return height_stiva == 0;
 }
 
