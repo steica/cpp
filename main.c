efe336cf6f287ff8871bdb10f4841a5bb5842495
Steica <steica@live.com>
2023-10-05 15:25:20 +0300

    PBInfo #852 Paranteze3

--- main.c
+++ main.c
@@ -1,57 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int stack[100], dim = 0;
-
-void push(int x) {
-    stack[dim++] = x;
-}
-
-void pop() {
-    dim--;
-    printf("%d ", stack[dim]);
-}
-
-int main() {
-    char str[256];
-    scanf("%s", str);
-    int ctr = 0;
-    for (int i = 0; str[i]; i++) {
-        if (str[i] == '(') {
-            ctr++;
-            push(ctr);
-        } else {
-            pop(dim);
-        }
-    }
-}
-
-
-/*#include <stdio.h>
-#include <string.h>
-
 int main(void) {
-    char str[255];
-    char stiva[255];
-    int rezultate[255], ind = 0;
-    scanf("%s", str);
-    int height_stiva = 0;
-    for (int j = 1; str[j]; j++) {
-        if (height_stiva == 0)
-            stiva[height_stiva++] = str[j];
-        else {
-            if (stiva[height_stiva - 1] == '(' && str[j] == ')') {
-                height_stiva--;
-                rezultate[ind++] =;
-            } else {
+    FILE *input, *output;
+    input = fopen("paranteze3.in", "r");
+    output = fopen("paranteze3.out", "w");
+    int n;
+    fscanf(input, "%d", &n);
+    char str[256];
+    char stiva[256];
+    for (int i = 1; i <= n; i++) {
+        fscanf(input, "%s", str);
+        int height_stiva = 0;
+        for (int j = 0; str[j]; j++) {
+            if (height_stiva == 0)
                 stiva[height_stiva++] = str[j];
+            else {
+                if (stiva[height_stiva - 1] == '(' && str[j] == ')' || stiva[height_stiva - 1] == '[' && str[j] == ']')
+                    height_stiva--;
+                else
+                    stiva[height_stiva++] = str[j];
             }
         }
+        if (height_stiva == 0)
+            fprintf(output, "1\n");
+        else
+            fprintf(output, "0\n");
+
     }
-    for (int i = 0; i < ind; i++)
-        printf("%d ", rezultate[i]);
-}*/
+    fclose(input);
+    fclose(output);
+}
+
+
 
 
 
