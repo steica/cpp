58871cd237a316611bea9f3abd3343c3dd97219b
Steica <steica@live.com>
2023-10-11 11:22:47 +0300

    PBInfo #897 SumCifF

--- main.c
+++ main.c
@@ -1,36 +1,14 @@
 #include <stdio.h>
-#include <string.h>
+
+long long sumcif(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n % 10 + sumcif(n / 10);
+}
 
 int main(void) {
-    FILE *input, *output;
-    input = fopen("ifelse.in", "r");
-    output = fopen("ifelse.out", "w");
-    int n;
-    char line[50002];
-    fscanf(input, "%d\n", &n);
-    for (int i = 0; i < n; i++) {
-        int nr = 0, cnt = 0, nr_cuv = 0;
-        fgets(line, sizeof(line), input);
-        char *word = strtok(line, " \n");
-        while (word) {
-            if (strcmp(word, "if") == 0)
-                nr++;
-            else {
-                nr--;
-                if (nr == - 1) {
-                    nr = 1;
-                    cnt++;
-                }
-
-            }
-            nr_cuv++;
-            word = strtok(NULL, " \n");
-        }
-        if (nr_cuv % 2 == 1)
-            fprintf(output, "-1\n");
-        else
-            fprintf(output, "%d\n", cnt + nr / 2);
-    }
-    fclose(input);
-    fclose(output);
+    int x;
+    scanf("%d", &x);
+    printf("%lld", sumcif(x));
 }
