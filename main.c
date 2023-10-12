6ea90167bef8f67217dab47171f12667dff214d1
Steica <steica@live.com>
2023-10-12 18:57:07 +0300

    PBInfo #67 AproapePrim

--- main.c
+++ main.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 
-int UCP(int n) {
-    if (n % 10 == 0)
+int prim(int n) {
+    if (n < 2)
         return 0;
-    while (n) {
-        if (n % 2 == 0)
-            return n % 10;
-        else
-            n /= 10;
+    if (n > 2 && n & 2 == 0)
+        return 0;
+    for (int d = 3; d * d <= n; d += 2)
+        if (n % d == 0)
+            return 0;
+    return 1;
+}
+
+int a_prim(int n) {
+    int cnt_diviz = 0;
+    for (int d = 2; d * d <= n; d++) {
+        if (n % d == 0)
+            cnt_diviz += 2;
+        if (d * d == n)
+            cnt_diviz--;
     }
-    return -1;
+    if (cnt_diviz == 2) {
+        int div1, div2;
+        for (int d = 2; d * d <= n; d++)
+            if (n % d == 0) {
+                div1 = d;
+                div2 = n / d;
+            }
+        if (prim(div1) && prim(div2))
+            return 1;
+    }
+    return 0;
 }
 
 
 int main(void) {
     int x;
     scanf("%d", &x);
-    printf("%d", UCP(x));
-}
+    printf("%d", prim(x));
+};
