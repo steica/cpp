bc06c28ed3146a668500c60ba83ba454d6c97a63
Steica <steica@live.com>
2023-10-12 17:56:20 +0300

    PBInfo #36 i_prim

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 int prim(int n) {
     if (n < 2)
         return 0;
-    else if (n == 2)
-        return 1;
+    if (n % 2 == 0 && n > 2)
+        return 0;
     else {
-        for (int d = 3; d * d <= n; d++) {
+        for (int d = 3; d * d <= n; d += 2) {
             if (n % d == 0)
                 return 0;
         }
@@ -14,8 +14,17 @@ int prim(int n) {
     return 1;
 }
 
+int i_prim(int n) {
+    int p1 = n, p2 = n;
+    while (!prim(p1))
+        p1--;
+    while (!prim(p2))
+        p2++;
+    return p2 - p1;
+}
+
 int main(void) {
-    long long x;
+    int x;
     scanf("%d", &x);
-    printf("%d", prim(x));
+    printf("%d", i_prim(x));
 }
