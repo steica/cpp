045d7b6348284f7ab96d0ba463e89183fd43041f
Steica <steica@live.com>
2023-10-12 18:23:37 +0300

    PBInfo #76 FUltimaCifraPara

--- main.c
+++ main.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 
-int prim(int n) {
-    if (n < 2)
+int UCP(int n) {
+    if (n % 10 == 0)
         return 0;
-    if (n % 2 == 0 && n > 2)
-        return 0;
-    else {
-        for (int d = 3; d * d <= n; d += 2) {
-            if (n % d == 0)
-                return 0;
-        }
+    while (n) {
+        if (n % 2 == 0)
+            return n % 10;
+        else
+            n /= 10;
     }
-    return 1;
+    return -1;
 }
 
-int i_prim(int n) {
-    int p1 = n, p2 = n;
-    while (!prim(p1))
-        p1--;
-    while (!prim(p2))
-        p2++;
-    return p2 - p1;
-}
 
 int main(void) {
     int x;
     scanf("%d", &x);
-    printf("%d", i_prim(x));
+    printf("%d", UCP(x));
 }
