3481383865af36421225ff92fea1412ce6c1bdd3
Steica <steica@live.com>
2023-10-13 13:07:41 +0300

    PBInfo #878 Intervale4

--- main.c
+++ main.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
-#include <math.h>
 
-int nz(int n){
-    int cnt5 = 0, cnt2 = 0;
-    int put2 = 2, put5 = 5;
-    while(put2 < n){
-        cnt2 += n / put2;
-        put2 *= 2;
-    }
-    while(put5 < n){
-        cnt5 += n / put5;
-        put5 *= 5;
-    }
-    return cnt5;
+struct interval {
+    int st, dr;
+};
+
+int isIntersect(struct interval a, struct interval b) {
+    if (a.st >= b.st && a.st <= b.dr)
+        return 1;
+    if (a.dr >= b.st && a.dr <= b.dr)
+        return 1;
+    if (b.st >= a.st && b.st <= a.dr)
+        return 1;
+    if (b.dr >= a.st && b.dr <= a.dr)
+        return 1;
+    return 0;
 }
 
 int main(void) {
-    int x;
-    scanf("%d", &x);
-    printf("%d", nz(x));
-};
+    FILE *input = fopen("intervale4.in", "r");
+    int n;
+    fscanf(input, "%d", &n);
+    struct interval stack[100001] = {0};
+    int h = 0;
+    for (int i = 0; i < n; i++) {
+        fscanf(input, "%d %d", &stack[h].st, &stack[h].dr);
+        while (h > 0 && isIntersect(stack[h - 1], stack[h])) {
+            if (stack[h - 1].st > stack[h].st)
+                stack[h - 1].st = stack[h].st;
+            if (stack[h - 1].dr < stack[h].dr)
+                stack[h - 1].dr = stack[h].dr;
+            h--;
+        }
+        h++;
+    }
+    fclose(input);
+    FILE *output = fopen("intervale4.out", "w");
+    fprintf(output, "%d", h);
+    fclose(output);
+}
+
+
