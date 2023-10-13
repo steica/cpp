2d0e177ea1e9d4c8ca96ef59a2ae6f022432ddf3
Steica <steica@live.com>
2023-10-13 14:12:08 +0300

    PBInfo #870 Depou

--- main.c
+++ main.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 
 int main(void) {
-    int k;
-    scanf("%d", &k);
-    char str[1000001];
-    scanf("%s", str);
-    char stack[1000001] = {0};
-    int height = 0;
-    for (int i = 0; str[i]; i++) {
-        while (height > 0 && k != 0 && stack[height - 1] > str[i]) {
-            height--;
-            k--;
+    int A[1000];
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &A[i]);
+    int B[1001] = {0};
+    int h_B = 0, h_C = 1;
+    int op[3000], ind = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (A[i] == h_C) {
+            h_C++;
+            op[ind++] = 1;
+            while (h_B > 0 && B[h_B - 1] == h_C) {
+                h_C++;
+                op[ind++] = 3;
+                h_B--;
+            }
+        } else {
+            B[h_B++] = A[i];
+            op[ind++] = 2;
         }
-        stack[height++] = str[i];
     }
-    height -= k;
-    stack[height] = '\0';
-    printf("%s", stack);
+    if (h_C != n + 1) {
+        printf("0\n");
+        return 0;
+    }
+    printf("%d\n", ind);
+    for (int i = 0; i < ind; i++) {
+        if (op[i] == 1)
+            printf("A C\n");
+        else if (op[i] == 2)
+            printf("A B\n");
+        else
+            printf("B C\n");
+    }
 }
 
 
